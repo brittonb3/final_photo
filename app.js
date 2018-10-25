849c57ecdd0fbaef2a98a50ea59b63f2387f40bc
Britton Baumann <brittonbaumann@gmail.com>
2018-10-25 17:19:18 -0500

    files added

--- app.js
+++ app.js
@@ -5,9 +5,34 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(expressLayouts);
 
+// set static files (css and images, ect) location
+app.use(express.static(__dirname + '/assets'));
+
 app.get('/', function (req, res) {
   res.render('pages/index')
 })
+
+app.get('/portfolio', function(req, res) {
+  res.render('pages/portfolio');
+});
+
+app.get('/blog', function(req, res) {
+  res.render('pages/blog');
+});
+
+app.get('/prints', function(req, res) {
+  res.render('pages/prints');
+});
+
+// routes for our about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('pages/contact');
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
