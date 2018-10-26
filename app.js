b4df68ec7d67bcb43c0223c971afce2e70822fd3
Britton Baumann <brittonbaumann@gmail.com>
2018-10-26 14:15:58 -0500

    fixed image bug on phones

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const path = require('path');
 
-app.set('views',path.join(__dirname,'views'));
+app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 app.use(expressLayouts);
 
